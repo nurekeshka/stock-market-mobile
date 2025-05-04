50c75cf04c424a44e6df73a11fb875ec9cae03f5
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2025-05-04 12:44:09 +0200

    feat: add recent changes on http

---
+++ lib/data/datasources/api_connector.dart
@@ -0,0 +1,27 @@
+import 'package:http/http.dart' as http;
+
+class ApiConnector {
+  final Uri api = Uri.parse('http://localhost:8080');
+
+  Future<T> get<T>(Uri route) async {
+    return http.get(api).then((response) {
+      if (response.statusCode == 200) {
+        return response.body as T;
+      } else {
+        throw Exception('Failed to load data');
+      }
+    });
+  }
+
+  somemeaningfulfunction() async {
+    final user = get<User>(api.resolve("/api/test"));
+    return user;
+  }
+}
+
+class User {
+  final String name;
+  final String email;
+
+  User({required this.name, required this.email});
+}
